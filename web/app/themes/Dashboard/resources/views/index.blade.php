c5ee0a4db2006eedbb3e6dfa83ead7c36f5d37a4
JimmyD <jdoussain@linaia.com>
2023-01-10 17:13:10 +0100

    Ajout Relation Taxonomie

--- web/app/themes/Dashboard/resources/views/index.blade.php
+++ web/app/themes/Dashboard/resources/views/index.blade.php
@@ -1,13 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-  <x-header/>
+    <x-header />
 
 
-  <x-specification />
+    <x-specification />
+    @php
 
-  {{-- Href to produit page --}}
+      $posts = get_posts(
+        array(
+          'post_type' => 'publication',
+          'posts_per_page' => 10,
+          'orderby' => 'date',
+          'order' => 'DESC',
+          "tax_query" => array(
+            array(
+              'taxonomy'=> 'langage',
+              'field' => 'name',
+              'terms' => 'javascript'
+            )
+          )
+        ));
+        
+      dd($posts);
+    @endphp
 
-  {!! get_the_posts_navigation() !!}
+
+
+    {{-- Href to produit page --}}
+
+    {!! get_the_posts_navigation() !!}
 @endsection
-
